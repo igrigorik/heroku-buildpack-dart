0364e0845de3a19204bdfc9e2231516601a916ae
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-03-04 21:48:14 +0100

    Update server code with dart:io V2

--- test-app/bin/main.dart
+++ test-app/bin/main.dart
@@ -2,21 +2,18 @@ import 'dart:io';
 import 'dart:json' as JSON;
 
 main() {
-  var server = new HttpServer();
   var port = int.parse(Platform.environment['PORT']);
-  server.listen('0.0.0.0', port);
-  print('Server started on port: ${port}');
-
-  server.defaultRequestHandler = (HttpRequest request, HttpResponse response) {
-
-    var resp = JSON.stringify({
-      'Dart on Heroku': true,
-      'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
-      'Environment': Platform.environment}
-    );
-
-    response.headers.set(HttpHeaders.CONTENT_TYPE, 'application/json');
-    response.outputStream.writeString(resp);
-    response.outputStream.close();
-  };
+  HttpServer.bind('0.0.0.0', port).then((HttpServer server){
+    print('Server started on port: ${port}');
+    server.listen((HttpRequest request) {
+      var resp = JSON.stringify({
+        'Dart on Heroku': true,
+        'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
+        'Environment': Platform.environment}
+      );
+      request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json')
+                      ..addString(resp)
+                      ..close();
+    });
+  });
 }
