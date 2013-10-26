b760d0245b973589ebed0e2f80ba05c8d1ba7f9e
Seth Ladd <sethladd@google.com>
2013-10-26 16:06:44 -0700

    update sample server

--- test-app/bin/main.dart
+++ test-app/bin/main.dart
@@ -1,19 +1,35 @@
 import 'dart:io';
-import 'dart:json' as JSON;
+import 'dart:async';
+import 'dart:convert' show JSON;
+import 'package:http_server/http_server.dart';
+import 'package:route/server.dart';
 
 main() {
-  var port = int.parse(Platform.environment['PORT']);
-  HttpServer.bind('0.0.0.0', port).then((HttpServer server) {
-    print('Server started on port: ${port}');
-    server.listen((HttpRequest request) {
-      var resp = JSON.stringify({
-        'Dart on Heroku': true,
-        'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
-        'Environment': Platform.environment}
-      );
-      request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json')
-                      ..write(resp)
-                      ..close();
+
+  String portEnv = Platform.environment['PORT'];
+  int port = portEnv == null ? 9999 : int.parse(portEnv);
+
+  runZoned(() {
+    HttpServer.bind('0.0.0.0', port).then((HttpServer server) {
+      print('Server started on port: ${port}');
+
+      new Router(server)
+        ..serve('/', method: 'GET').listen(sayHi);
     });
+  },
+  onError: (e, stackTrace) {
+    print('Error: $e $stackTrace');
   });
+
+}
+
+void sayHi(HttpRequest request) {
+  var resp = JSON.encode({
+    'Dart on Heroku': true,
+    'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
+    'Environment': Platform.environment}
+  );
+  request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json')
+                  ..write(resp)
+                  ..close();
 }
